13b134f9a2fdef033a6150f4849c24b0a7925454
Cria-Hub-2 <126715867+WhoppDev@users.noreply.github.com>
2024-03-25 14:54:35 -0300

    Att Montanha Russa

--- CRIA MUNDO - Teste/Assets/GAME/Scripts/RodaGigante.cs
+++ CRIA MUNDO - Teste/Assets/GAME/Scripts/RodaGigante.cs
@@ -4,10 +4,18 @@ using UnityEngine;
 
 public class RodaGigante : MonoBehaviour
 {
-    public float rotationSpeed = 10.0f; 
+    public float rotationSpeed = 10.0f;
+    public Transform[] seats;
 
     void Update()
     {
-        transform.Rotate(0, rotationSpeed * Time.deltaTime, 0f);
+        transform.Rotate(0, rotationSpeed * Time.deltaTime, 0f); //Rotaciona a Montanha Russa
+
+        foreach (Transform seat in seats) //Rotaciona os bancos em relação a Montanha Russa
+        {
+            Quaternion desiredRotation = Quaternion.Euler(-90, transform.eulerAngles.y, 50);
+            seat.rotation = desiredRotation;
+            seat.Rotate(0, 0, transform.eulerAngles.y);
+        }
     }
 }
